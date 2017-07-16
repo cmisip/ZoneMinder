40c6a16980b982c90fab5efed98ebf6e5bbcc12a
Chris <cmisip@gmail.com>
2017-07-16 17:14:15 -0400

    Temporarily disable neon stuff in zm_image.cpp so we can make changes

--- src/zm_image.cpp
+++ src/zm_image.cpp
@@ -26,6 +26,8 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#define ZM_STRIP_NEON
+
 static unsigned char y_table_global[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 55, 57, 58, 59, 60, 61, 62, 64, 65, 66, 67, 68, 69, 71, 72, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 93, 94, 95, 96, 97, 98, 100, 101, 102, 103, 104, 105, 107, 108, 109, 110, 111, 112, 114, 115, 116, 117, 118, 119, 121, 122, 123, 124, 125, 126, 128, 129, 130, 131, 132, 133, 135, 136, 137, 138, 139, 140, 142, 143, 144, 145, 146, 147, 149, 150, 151, 152, 153, 154, 156, 157, 158, 159, 160, 161, 163, 164, 165, 166, 167, 168, 170, 171, 172, 173, 174, 175, 176, 178, 179, 180, 181, 182, 183, 185, 186, 187, 188, 189, 190, 192, 193, 194, 195, 196, 197, 199, 200, 201, 202, 203, 204, 206, 207, 208, 209, 210, 211, 213, 214, 215, 216, 217, 218, 220, 221, 222, 223, 224, 225, 227, 228, 229, 230, 231, 232, 234, 235, 236, 237, 238, 239, 241, 242, 243, 244, 245, 246, 248, 249, 250, 251, 252, 253, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255};
 
 static signed char uv_table_global[] = {-127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -127, -125, -124, -123, -122, -121, -120, -119, -117, -116, -115, -114, -113, -112, -111, -109, -108, -107, -106, -105, -104, -103, -102, -100, -99, -98, -97, -96, -95, -94, -92, -91, -90, -89, -88, -87, -86, -85, -83, -82, -81, -80, -79, -78, -77, -75, -74, -73, -72, -71, -70, -69, -68, -66, -65, -64, -63, -62, -61, -60, -58, -57, -56, -55, -54, -53, -52, -51, -49, -48, -47, -46, -45, -44, -43, -41, -40, -39, -38, -37, -36, -35, -34, -32, -31, -30, -29, -28, -27, -26, -24, -23, -22, -21, -20, -19, -18, -17, -15, -14, -13, -12, -11, -10, -9, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 51, 52, 53, 54, 55, 56, 57, 58, 60, 61, 62, 63, 64, 65, 66, 68, 69, 70, 71, 72, 73, 74, 75, 77, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 94, 95, 96, 97, 98, 99, 100, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 120, 121, 122, 123, 124, 125, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127};
@@ -218,18 +220,6 @@ void Image::Initialise()
     if(config.cpu_extensions && sseversion >= 20) {
       fptr_blend = &sse2_fastblend; /* SSE2 fast blend */
       Debug(4,"Blend: Using SSE2 fast blend function");
-    } else if(config.cpu_extensions && neonversion >= 1) {
-//#if defined(__aarch64__)
-#if (defined(__aarch64__) && !defined(ZM_STRIP_NEON))
-      fptr_blend = &neon64_armv8_fastblend;  /* ARM Neon (AArch64) fast blend */
-      Debug(4,"Blend: Using ARM Neon (AArch64) fast blend function");
-//#elif defined(__arm__)
-#elif (defined(__arm__) && !defined(ZM_STRIP_NEON))
-      fptr_blend = &neon32_armv7_fastblend;  /* ARM Neon (AArch32) fast blend */
-      Debug(4,"Blend: Using ARM Neon (AArch32) fast blend function");
-//#else
-//      Panic("Bug: Non ARM platform but neon present");
-#endif
     } else {
       fptr_blend = &std_fastblend;  /* standard fast blend */
       Debug(4,"Blend: Using fast blend function");
@@ -290,28 +280,7 @@ void Image::Initialise()
       fptr_delta8_abgr = &sse2_delta8_abgr;
       fptr_delta8_gray8 = &sse2_delta8_gray8;
       Debug(4,"Delta: Using SSE2 delta functions");
-    } else if(neonversion >= 1) {
-      /* ARM Neon available */
-//#if defined(__aarch64__)
-#if (defined(__aarch64__) && !defined(ZM_STRIP_NEON))
-      fptr_delta8_rgba = &neon64_armv8_delta8_rgba;
-      fptr_delta8_bgra = &neon64_armv8_delta8_bgra;
-      fptr_delta8_argb = &neon64_armv8_delta8_argb;
-      fptr_delta8_abgr = &neon64_armv8_delta8_abgr;
-      fptr_delta8_gray8 = &neon64_armv8_delta8_gray8;
-      Debug(4,"Delta: Using ARM Neon (AArch64) delta functions");
-//#elif defined(__arm__)
-#elif (defined(__arm__) && !defined(ZM_STRIP_NEON))
-      fptr_delta8_rgba = &neon32_armv7_delta8_rgba;
-      fptr_delta8_bgra = &neon32_armv7_delta8_bgra;
-      fptr_delta8_argb = &neon32_armv7_delta8_argb;
-      fptr_delta8_abgr = &neon32_armv7_delta8_abgr;
-      fptr_delta8_gray8 = &neon32_armv7_delta8_gray8;
-      Debug(4,"Delta: Using ARM Neon (AArch32) delta functions");
-//#else
-//      Panic("Bug: Non ARM platform but neon present");
-#endif
-    } else {
+    }  else {
       /* No suitable SSE version available */
       fptr_delta8_rgba = &std_delta8_rgba;
       fptr_delta8_bgra = &std_delta8_bgra;
