7cab2367c8a73aba474dab1171419207f2a46885
Chris <cmisip@gmail.com>
2017-08-01 18:29:49 -0400

    Fixes

--- src/zm_ffmpeg_camera.cpp
+++ src/zm_ffmpeg_camera.cpp
@@ -438,20 +438,24 @@ if (ctype) { //motion vectors from hardware h264 encoding on the RPI only, the s
                     }
                     mmal_buffer_header_mem_unlock(rbuffer);   
                     
-                    mmal_buffer_header_release(rbuffer);
-                    
-                   // if (received)
-                      //  break;
-                    if ((rbuffer = mmal_queue_get(pool_outr->queue)) != NULL) {
-                           if (mmal_port_send_buffer(resizer->output[0], rbuffer) != MMAL_SUCCESS) {
-                              goto end;
-                           } 
-                    }         
-                    
                 }
                 
                 if (!received) {
-                    Fatal("Did not receive the RGB Data");
+                    if (mmal_port_send_buffer(resizer->input[0], rbuffer) != MMAL_SUCCESS) {
+                       Warning("failed to resend YUV420 buffer to resizer for frame %d\n", frameCount);
+                       goto end;
+                } else {
+                   mmal_buffer_header_release(rbuffer);
+                   
+                   if ((rbuffer = mmal_queue_get(pool_outr->queue)) != NULL) {
+                           if (mmal_port_send_buffer(resizer->output[0], rbuffer) != MMAL_SUCCESS) {
+                              goto end;
+                           } 
+                   }         
+                   
+                   
+                }    
+                    //Fatal("Did not receive the RGB Data");
                 }
                 
 } //if ctype
