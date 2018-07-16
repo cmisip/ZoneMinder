1879989bcfd7df56350409295082717998360966
Christopher Isip <cmisip@gmail.com>
2018-07-15 20:07:20 -0400

    Removed one pointless memcpy

--- src/zm_ffmpeg_camera.cpp
+++ src/zm_ffmpeg_camera.cpp
@@ -459,7 +459,7 @@ if (ctype) { //motion vectors from hardware h264 encoding on the RPI only, the s
                         
                         //copy buffer->data to temporary
                         memcpy(mvarray,buffer->data,buffer->length);
-                        memset(mvect_buffer,buffer->length,sizeof(buffer->length));
+                        //memset(mvect_buffer,buffer->length,sizeof(buffer->length));//WHY
                         mmal_buffer_header_mem_unlock(buffer);   
                         
                         for (int i=0;i < size ; i++) {
